53d2eba2dd88c363026f7fb5cc1e34830e98391d
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-01-02 21:12:11 +0600

    PostgreSQL driver installed and configured

--- realstate/settings.py
+++ realstate/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'realstate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btre',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
