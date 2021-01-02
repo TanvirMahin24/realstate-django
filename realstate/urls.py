00910ee84f15016a50cea58932d8e68868d52942
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-01-02 20:37:53 +0600

    listing app added with frontend

--- realstate/urls.py
+++ realstate/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listing/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
