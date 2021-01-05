38266c149623f8a610a31377b76d6dad1085eef4
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-01-05 18:35:18 +0600

    Register and Login frontend added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- realstate/urls.py
+++ realstate/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
     path('listing/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
