e94467d8a4fce4f3b4cd31430a31be1e5d0561c4
TanvirMahin24 <tanvirmahin24@gmail.com>
2021-01-11 18:51:51 +0600

    inquery submittion done

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import Contact
 
-# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        #Check already made inquery
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_inquery = Contact.objects.all().filter(listing_id=listing_id,user_id=user_id)
+
+            if has_inquery:
+                messages.error(request,"You have already made an inquery for this listing !")
+                return redirect('/listing/'+listing_id)
+
+        contact = Contact(listing=listing,listing_id=listing_id,name=name,email=email,phone=phone,message=message,user_id=user_id)
+        contact.save()
+
+        messages.success(request, "Your request is submitted, a realtor will get back to you soon !")
+
+        return redirect('/listing/'+listing_id)
--- realstate/urls.py
+++ realstate/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('listing/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
